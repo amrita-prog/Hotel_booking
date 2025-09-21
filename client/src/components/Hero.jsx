077b03512839723f30515e890cfa8f52fab676a9
Amrita Mishra <itizamritamishra@gmail.com>
2025-09-21 23:49:41 +0530

    Add heroImage asset and update Hero component background style

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -3,8 +3,11 @@ import { assets, cities } from '../assets/assets'
 
 
 const Hero = () => {
-  return (
-    <div className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-[url("src/assets/heroImage.png")] bg-no-repeat bg-cover bg-center h-screen'>
+    return (
+        <div
+            className='flex flex-col items-start justify-center px-6 md:px-16 lg:px-24 xl:px-32 text-white bg-no-repeat bg-cover bg-center h-screen'
+            style={{ backgroundImage: `url(${assets.heroImage})` }}
+        >
 
       <p className='bg-[#49B9FF]/50 px-3.5 py-1 rounded-full mt-20'>The Ultimate Hotel Experience</p>
 
