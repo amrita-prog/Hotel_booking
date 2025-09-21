7145dda246281be15431ff14efd6142974ef7e01
Amrita Mishra <itizamritamishra@gmail.com>
2025-09-21 23:27:17 +0530

    update

--- client/src/pages/RoomDetails.jsx
+++ client/src/pages/RoomDetails.jsx
@@ -8,22 +8,19 @@ const RoomDetails = () => {
   const [mainImage, setMainImage] = useState(null)
 
   useEffect(() => {
-    const foundRoom = roomsDummyData.find(r => r.id === parseInt(id))
+    const foundRoom = roomsDummyData.find(r => r._id === id)
     if (foundRoom) {
       setRoom(foundRoom)
       setMainImage(foundRoom.images?.[0] || null)
     }
   }, [id])
 
-  if (!room) {
-    return <p className="py-28 text-center text-gray-500">Room not found.</p>
-  }
 
-  return (
+  return room && (
     <div className='py-28 md:py-35 px-4 md:px-16 lg:px-24 xl:px-32'>
       <div className='flex flex-col md:flex-row gap-2 items-start md:items-center'>
         <h1 className='text-3xl md:text-4xl font-playfair'>
-          {room.hotel.name}{' '}
+          {room.hotel.name}
           <span className='font-inner text-sm'>({room.roomType})</span>
         </h1>
         <p className='text-xs font-inter py-1.5 px-3 text-white bg-orange-500 rounded-full'>
